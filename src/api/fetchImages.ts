c6e91db5d5fb13f7e17a29136bd9fb933db73355
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-04-03 20:58:25 +0200

    check mistakes

--- src/api/fetchImages.ts
+++ src/api/fetchImages.ts
@@ -7,7 +7,7 @@ export interface Image {
 }
 
 export const fetchImages = async (query: string, page: number): Promise<Image[]> => {
-  const API_KEY = "YOUR_API_KEY";
+  const API_KEY = "Gd4hAjZGY4iB_bM6T4t-GkWy6P1FguQKr-0JqonIA6k";
   const response = await fetch(
     `https://api.unsplash.com/search/photos?query=${query}&page=${page}&client_id=${API_KEY}`
   );
